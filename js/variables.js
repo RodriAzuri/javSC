3f1fad0f30fabfab0174f99031e1f1e9e1cc77dc
Rodri Azuri <azuri.rodrigo@gmail.com>
2021-10-24 03:35:14 -0300

    first commit

---
+++ js/variables.js
@@ -0,0 +1,8 @@
+let nombreUsuario = prompt("Diganos su nombre");
+let emailUsuario = prompt("Diganos su e-mail");
+
+
+console.log("Hola " + nombreUsuario);
+console.log("Su e-mail es " + emailUsuario);
+
+alert("Muchas gracias, estaremos enviando información al siguiente correo:" + " " + emailUsuario);
